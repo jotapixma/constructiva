ba265235e77df55557caaca007ecc9044b807c2f
jotapixma <jorgeteran22@gmail.com>
2024-02-27 13:06:24 -0300

    fix scroll page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,14 @@
 import Layout from '../components/Layouts/Layout/Layout';
-import { Inter } from 'next/font/google'
-import Hero from '../components/Hero/Hero'
-import Services from '../components/Services/Services'
-import Purpose from '../components/Purpose/Purpose'
-import Partners from '../components/Partners/Partners'
-import Support from '../components/Support/Support'
-import ContactForm from '../components/ContactForm/ContactForm';
-import styles from '@/styles/Home.module.css'
+import Home from '../components/Home/Home';
 
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function index() {
   return (
     <>
-      <main className={styles.main}>
+      <main>
         <Layout>
-          <Hero/>
-          <Support title="Te apoyamos en las diferentes áreas de tu empresa"/>
-          <Services title="Servicios"/>
-          <Purpose title="Nuestro propósito"/>
-          <Partners title="Han confiado en nosotros"/>
-          <ContactForm title="Contactános"/>
+          <Home/>
         </Layout>
       </main>
     </>
